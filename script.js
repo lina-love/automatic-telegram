c0b2cf29510670b03c2cbc646e34064f822a85dd
lina-love <144587988+lina-love@users.noreply.github.com>
2025-05-14 09:25:24 +0000

    ddddd

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function handleClick() {
       event_label: "Hello Button"
     });
 }
+
+function handleClick2() {
+    gtag("event", "button_click_again", {
+      event_category: "User",
+      event_label: "Hello Kinawy"
+    });
+}
