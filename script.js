c8b2c44c4935c24cd52c1d5320c4c2b5c8d1ab0c
lina-love <144587988+lina-love@users.noreply.github.com>
2025-05-14 09:21:01 +0000

    d

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 function handleClick() {
-  if (
-    window.Usercentrics
-  ) {
     gtag("event", "button_click", {
       event_category: "User",
       event_label: "Hello Button"
     });
-  } else {
-    console.log("Consent not granted. Event not sent.");
-  }
 }
